e507127ac9fda8a9872bdc8ab7c727798377a9c4
Nelson <Nelson@Nelson-PC>
2018-12-04 18:53:58 -0800

    Version 1.30 Added Search Documents page
    Search now has query bar, clear cutton, results selection and display
    section
    
    set up variables for class-wide use

--- PirexProject/src/gui/MainInterface.java
+++ PirexProject/src/gui/MainInterface.java
@@ -4,18 +4,28 @@ package gui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener; //added for possible future use
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.GridLayout;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.Border;
 import javax.swing.JTabbedPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.ScrollPaneConstants;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JComponent;
+import javax.swing.JLabel;
 import javax.swing.border.EmptyBorder;
  
 
 public class MainInterface extends JPanel {
+	private String querySearch;
+	private JTextField queryInput;
+	private JTextArea midText;
+	private JTextArea bottomText;
+	private String nameOfOpus;
 	
 	public MainInterface() {
 		super(new GridLayout(1, 1));
@@ -38,8 +48,67 @@ public class MainInterface extends JPanel {
 		
 	}
 	private JComponent searchTab() {
-        JPanel panel = new JPanel(new BorderLayout());
+		JPanel panel = new JPanel(new BorderLayout());
         panel.setBorder(new EmptyBorder(15,10,10,10));
+        
+        JPanel topPanel = new JPanel(new BorderLayout());
+        topPanel.setBorder(new EmptyBorder(5,10,10,10));
+        JLabel query = new JLabel();
+        query.setText("Query");
+        query.setBorder(new EmptyBorder(0,0,0,5));
+        topPanel.add(query, BorderLayout.WEST);
+        queryInput = new JTextField(15);
+        
+        Border outline = BorderFactory.createLineBorder(Color.BLACK);
+        queryInput.setBorder(BorderFactory.createCompoundBorder(outline, BorderFactory.createEmptyBorder(5, 5, 0, 5)));
+        
+        topPanel.add(queryInput, BorderLayout.CENTER);
+        
+        JButton clear = new JButton("Clear");
+        
+        clear.addActionListener(new ActionListener(){
+        	public void actionPerformed(ActionEvent e) {
+        		queryInput.setText("");
+        	}
+        });
+        
+        topPanel.add(clear, BorderLayout.EAST);
+        
+        panel.add(topPanel, BorderLayout.NORTH);
+        JPanel midDisplay = new JPanel();
+        midText = new JTextArea(15, 90);
+        midText.setEditable(false);
+        midDisplay.add(midText);
+        JScrollPane midScroll = new JScrollPane ( midText );
+        midText.setWrapStyleWord(true);
+        midScroll.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED );
+        midDisplay.add(midScroll);
+        
+        
+        
+        
+        panel.add(midDisplay);
+        
+        JPanel bottomDisplay = new JPanel();
+        bottomText = new JTextArea(15, 90);
+        bottomText.setEditable(false);
+        
+        bottomDisplay.add(bottomText);
+        
+        JScrollPane bottomScroll = new JScrollPane ( bottomText );
+        bottomScroll.setVerticalScrollBarPolicy ( ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS );
+        bottomDisplay.add(bottomScroll);
+        
+        panel.add(bottomDisplay, BorderLayout.SOUTH);
+        queryInput.addActionListener(new ActionListener() {						// action listener for query input the method inside will activate on pressing ENTER key
+        	public void actionPerformed(ActionEvent e) {
+        			
+        			querySearch = queryInput.getText();
+        			
+        			midText.setText("I see that you are searching but the search function is not currently implemented yet \n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n Testing as needed scrollbar");
+        			bottomText.setText("I see that you are searching but the search function is not currently implemented yet ");
+        	}
+        });
         
         return panel;
     }
